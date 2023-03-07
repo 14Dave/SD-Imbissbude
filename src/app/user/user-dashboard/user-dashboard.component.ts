0c71cc54fe735b422b35b2b4e69534cdfaf69a22
14Dave <89302479+14Dave@users.noreply.github.com>
2023-03-07 11:15:19 +0100

    add dashboard

--- src/app/user/user-dashboard/user-dashboard.component.ts
+++ src/app/user/user-dashboard/user-dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { Dish } from 'src/app/model/dish';
 import { AuthService } from 'src/app/shared/auth.service';
+import { DataService } from 'src/app/shared/data.service';
 
 @Component({
   selector: 'app-user-dashboard',
@@ -7,7 +9,40 @@ import { AuthService } from 'src/app/shared/auth.service';
   styleUrls: ['./user-dashboard.component.css'],
 })
 export class UserDashboardComponent {
-  constructor(public authService: AuthService) {
-    console.log(localStorage.getItem('user'));
+  dishList: Dish[] = [];
+  dishObj: Dish = {
+    id: '',
+    name: '',
+    price: '',
+    description: '',
+    pictureLink: '',
+  };
+  keys: number[] = [];
+  key: number = 0;
+
+  constructor(private auth: AuthService, private data: DataService) {}
+
+  ngOnInit(): void {
+    this.getAllDishes();
+  }
+
+  getAllDishes() {
+    this.data.getAllDishes().subscribe(
+      (res) => {
+        this.dishList = res.map((e: any) => {
+          const data = e.payload.doc.data();
+          data.id = e.payload.doc.id;
+          return data;
+        });
+      },
+      (err) => {
+        alert(err.message);
+      }
+    );
+  }
+
+  addToCard(dish: Dish) {
+    localStorage.setItem(this.key.toString(), dish.id);
+    this.key += 1;
   }
 }
