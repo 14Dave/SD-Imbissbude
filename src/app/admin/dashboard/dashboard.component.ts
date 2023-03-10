25e5f05ade3ac24fc9af2b5fc7f1fa22139c0b9e
Timon Wiens <timon.wiens@bst.group>
2023-03-10 12:10:11 +0100

    state yet

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from 'src/app/shared/data.service';
+import { Ordered } from 'src/app/model/ordered'
 
 @Component({
   selector: 'app-dashboard',
@@ -7,19 +8,24 @@ import { DataService } from 'src/app/shared/data.service';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-// orderedList: Ordered[] = [];
-// orderedObj: Ordered = {
-//   username: '',
-//   orderedDishes: '',
-//   pickupTime: '',
-// };
-// username: string = 'wiet';
-// orderedDishes: string = 'Döner, Döner, Pizza';
-// pickupTime: string = '20230308213812';
-// accept: boolean = false;
+orderedList: Ordered[] = [];
+orderedObj: Ordered = {
+  username: '',
+  orderTime: '',
+  orderedDishes: '',
+  totalPrice: '',
+  pickupTime: '',
+  accept: false,
+};
+username: string = 'wiet';
+orderTime: string = '20230310111923';
+orderedDishes: string = 'Döner, Döner, Pizza';
+totalPrice: string = '32€';
+pickupTime: string = '20230312213812';
+accept: boolean = false;
 
 
-// constructor(private data: DataService){}
+constructor(private data: DataService){}
 
 ngOnInit(): void {
   // this.getAllOrdered();
--- src/app/model/ordered.ts
+++ src/app/model/ordered.ts
@@ -2,6 +2,7 @@ export interface Ordered {
     username: string;
     orderTime: string;
     orderedDishes: string;
+    totalPrice: string;
     pickupTime: string;
     accept: boolean;
 }
